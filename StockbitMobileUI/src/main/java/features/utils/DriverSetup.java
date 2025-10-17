a1c942d59d7fdb2acb701ad313933617eea5b5d1
mutiarahmatun <mutia.husnarahmatun@gmail.com>
2025-10-17 22:54:25 +0700

    remove unnecessary file

--- StockbitMobileUI/src/main/java/features/utils/DriverSetup.java
+++ StockbitMobileUI/src/main/java/features/utils/DriverSetup.java
@@ -12,7 +12,7 @@ public class DriverSetup {
         caps.setCapability("deviceName", "emulator-5554");
         caps.setCapability("app", System.getProperty("user.dir") + "/app/mda-1.0.13-15.apk");
         caps.setCapability("automationName", "UiAutomator2");
-        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), caps);
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), caps);
     }
     public static AppiumDriver getDriver() { return driver; }
 }
